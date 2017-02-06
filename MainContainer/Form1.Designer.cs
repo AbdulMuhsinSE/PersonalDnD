1cfb1791ca788a4f18d10a7e93091b06580878df
AbdulMuhsin J. Al-Kandari <abdulmuhsin.alkandari@outlook.com>
2017-02-06 17:07:35 -0500

    First Real Commit
    Setting up the project, and added the first die rolls for the d4 and d6

---
+++ MainContainer/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace MainContainer
+{
+    partial class MainContainer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.PagedControls = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.d6Button = new System.Windows.Forms.Button();
+            this.dicePictureBox = new System.Windows.Forms.PictureBox();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.mainMenuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveAsToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exportToToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tabsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.addTabToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.d4Button = new System.Windows.Forms.Button();
+            this.PagedControls.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dicePictureBox)).BeginInit();
+            this.mainMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PagedControls
+            // 
+            this.PagedControls.Controls.Add(this.tabPage1);
+            this.PagedControls.Controls.Add(this.tabPage2);
+            this.PagedControls.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.PagedControls.Location = new System.Drawing.Point(0, 24);
+            this.PagedControls.Name = "PagedControls";
+            this.PagedControls.SelectedIndex = 0;
+            this.PagedControls.Size = new System.Drawing.Size(723, 438);
+            this.PagedControls.SizeMode = System.Windows.Forms.TabSizeMode.FillToRight;
+            this.PagedControls.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.d4Button);
+            this.tabPage1.Controls.Add(this.d6Button);
+            this.tabPage1.Controls.Add(this.dicePictureBox);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(715, 412);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // d6Button
+            // 
+            this.d6Button.Location = new System.Drawing.Point(357, 102);
+            this.d6Button.Name = "d6Button";
+            this.d6Button.Size = new System.Drawing.Size(75, 23);
+            this.d6Button.TabIndex = 1;
+            this.d6Button.Text = "d6";
+            this.d6Button.UseVisualStyleBackColor = true;
+            this.d6Button.Click += new System.EventHandler(this.d6Button_Click);
+            // 
+            // dicePictureBox
+            // 
+            this.dicePictureBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dicePictureBox.Location = new System.Drawing.Point(438, 70);
+            this.dicePictureBox.Name = "dicePictureBox";
+            this.dicePictureBox.Size = new System.Drawing.Size(244, 259);
+            this.dicePictureBox.TabIndex = 0;
+            this.dicePictureBox.TabStop = false;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(715, 412);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // mainMenuStrip
+            // 
+            this.mainMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.tabsToolStripMenuItem});
+            this.mainMenuStrip.Location = new System.Drawing.Point(0, 0);
+            this.mainMenuStrip.Name = "mainMenuStrip";
+            this.mainMenuStrip.Size = new System.Drawing.Size(723, 24);
+            this.mainMenuStrip.TabIndex = 1;
+            this.mainMenuStrip.Text = "mainMenuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.saveToolStripMenuItem,
+            this.saveAsToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.saveToolStripMenuItem1,
+            this.saveAsToolStripMenuItem1,
+            this.exportToToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            this.fileToolStripMenuItem.Click += new System.EventHandler(this.fileToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem
+            // 
+            this.saveAsToolStripMenuItem.Name = "saveAsToolStripMenuItem";
+            this.saveAsToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.saveAsToolStripMenuItem.Text = "New";
+            // 
+            // loadToolStripMenuItem
+            // 
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.loadToolStripMenuItem.Text = "Load";
+            // 
+            // saveToolStripMenuItem1
+            // 
+            this.saveToolStripMenuItem1.Name = "saveToolStripMenuItem1";
+            this.saveToolStripMenuItem1.Size = new System.Drawing.Size(123, 22);
+            this.saveToolStripMenuItem1.Text = "Save";
+            // 
+            // saveAsToolStripMenuItem1
+            // 
+            this.saveAsToolStripMenuItem1.Name = "saveAsToolStripMenuItem1";
+            this.saveAsToolStripMenuItem1.Size = new System.Drawing.Size(123, 22);
+            this.saveAsToolStripMenuItem1.Text = "Save As";
+            // 
+            // exportToToolStripMenuItem
+            // 
+            this.exportToToolStripMenuItem.Name = "exportToToolStripMenuItem";
+            this.exportToToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.exportToToolStripMenuItem.Text = "Export To";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(123, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            // 
+            // tabsToolStripMenuItem
+            // 
+            this.tabsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addTabToolStripMenuItem});
+            this.tabsToolStripMenuItem.Name = "tabsToolStripMenuItem";
+            this.tabsToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.tabsToolStripMenuItem.Text = "Tabs";
+            // 
+            // addTabToolStripMenuItem
+            // 
+            this.addTabToolStripMenuItem.Name = "addTabToolStripMenuItem";
+            this.addTabToolStripMenuItem.Size = new System.Drawing.Size(144, 22);
+            this.addTabToolStripMenuItem.Text = "Manage Tabs";
+            // 
+            // d4Button
+            // 
+            this.d4Button.Location = new System.Drawing.Point(357, 73);
+            this.d4Button.Name = "d4Button";
+            this.d4Button.Size = new System.Drawing.Size(75, 23);
+            this.d4Button.TabIndex = 2;
+            this.d4Button.Text = "d4";
+            this.d4Button.UseVisualStyleBackColor = true;
+            this.d4Button.Click += new System.EventHandler(this.d4Button_Click);
+            // 
+            // MainContainer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(723, 462);
+            this.Controls.Add(this.PagedControls);
+            this.Controls.Add(this.mainMenuStrip);
+            this.Name = "MainContainer";
+            this.Text = "D&DGameChanger";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.PagedControls.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dicePictureBox)).EndInit();
+            this.mainMenuStrip.ResumeLayout(false);
+            this.mainMenuStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        public System.Windows.Forms.TabControl PagedControls;
+        private System.Windows.Forms.PictureBox dicePictureBox;
+        private System.Windows.Forms.MenuStrip mainMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem;
+        private System.Windows.Forms.Button d6Button;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem saveAsToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem exportToToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem tabsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem addTabToolStripMenuItem;
+        private System.Windows.Forms.Button d4Button;
+    }
+}
+
---
+++ MainContainer/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainContainer
+{
+    public partial class MainContainer : Form
+    {
+        Random rng;
+        int dieRoll;
+        int forThefakes = 0;
+
+        public MainContainer()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void fileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void d6Button_Click(object sender, EventArgs e)
+        {
+            rng = new Random();
+            dieRoll = rng.Next(1, 7);
+            //fake a one lulz
+            if(forThefakes == 0)
+            {
+                dieRoll = 1;
+                forThefakes++;
+            }
+            dicePictureBox.SizeMode = PictureBoxSizeMode.CenterImage;
+            switch (dieRoll)
+            {
+                case 1:
+                    {
+                        dicePictureBox.Image = Properties.Resources.nat1;
+                        break;
+                    }
+                default :
+                    {
+                        MessageBox.Show("Transforming pizza to code... Please wait. \n "+ dieRoll+".", "The programmer made an oopsie", MessageBoxButtons.OK);
+                        break;
+                    }
+            }
+            dicePictureBox.Refresh();
+        }
+
+        private void d4Button_Click(object sender, EventArgs e)
+        {
+            rng = new Random();
+            dieRoll = rng.Next(1, 5);
+            //fake a one lulz
+            if (forThefakes == 0)
+            {
+                dieRoll = 1;
+                forThefakes++;
+            }
+            dicePictureBox.SizeMode = PictureBoxSizeMode.CenterImage;
+            switch (dieRoll)
+            {
+                case 1:
+                    {
+                        dicePictureBox.Image = Properties.Resources.nat1;
+                        break;
+                    }
+                default:
+                    {
+                        MessageBox.Show("Transforming pizza to code... Please wait.", "The programmer made an oopsie", MessageBoxButtons.OK);
+                        break;
+                    }
+            }
+            dicePictureBox.Refresh();
+        }
+    }
+}
