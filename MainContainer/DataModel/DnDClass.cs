c124f988dd9f0e98102d5da449f5df43db3bacda
AbdulMuhsin J. Al-Kandari <abdulmuhsin.alkandari@outlook.com>
2017-02-11 21:17:22 -0500

    Story #13: Development of data model classes to remove block on character model class

---
+++ MainContainer/DataModel/DnDClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainContainer.DataModel
+{
+    class DnDClass
+    {
+        Tuple<int, DNDUtils.Dice> hitDice;
+        Tuple<int, Attribute> hp1stLevel;
+        Tuple<DNDUtils.Dice, Attribute> hpHigherLevel;
+
+        /********************************************
+         **INCOMPLETE********************************
+         ********************************************
+         *******************************************/ 
+    }
+}
---
+++ MainContainer/DataModel/items.cs
@@ -0,0 +1,112 @@
+﻿using System;
+
+namespace MainContainer.DataModel
+{
+    class Items
+    {
+        string name;
+        string description;
+        bool consumable;
+        NullableBoolean hpEffect;
+        int hpEffectValue;
+        int val; // val of 0 means the item cannot be sold. val of 1 = 1cp
+
+        public Items(string name, string description)
+        {
+            this.name = name;
+            this.description = description;
+            this.consumable = false;
+            this.hpEffect = NullableBoolean.Null;
+            this.val = 0;
+            this.hpEffectValue = 0;
+        }
+
+        public Items(string name, string description, bool consumable, NullableBoolean hpEffect, int hpEffectValue, int val)
+        {
+            this.name = name;
+            this.description = description;
+            this.consumable = consumable;
+            this.hpEffect = hpEffect;
+            this.hpEffectValue = hpEffectValue;
+            this.val = val;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                description = value;
+            }
+        }
+
+        public bool Consumable
+        {
+            get
+            {
+                return consumable;
+            }
+
+            set
+            {
+                consumable = value;
+            }
+        }
+
+        internal NullableBoolean HpEffect
+        {
+            get
+            {
+                return hpEffect;
+            }
+
+            set
+            {
+                hpEffect = value;
+            }
+        }
+
+        public int HpEffectValue
+        {
+            get
+            {
+                return hpEffectValue;
+            }
+
+            set
+            {
+                hpEffectValue = value;
+            }
+        }
+
+        public int Val
+        {
+            get
+            {
+                return val;
+            }
+
+            set
+            {
+                val = value;
+            }
+        }
+    }
+}
